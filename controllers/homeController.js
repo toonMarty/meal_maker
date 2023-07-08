0f2bb6ec2da9cb80cfdb413d974cf278e10d98e3
toonMarty <martinnyaga7373@gmail.com>
2023-07-08 15:56:24 +0300

    Passing dynamic content to views

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,31 @@
 /*callback functions for specific routes */
+let courses = [
+  {
+    title : 'Asynchronous Muffins',
+    cost  : 200
+  },
+
+  {
+    title : 'Agile Mandelbrot Cookies',
+    cost  : 100
+  },
+
+  {
+    title : 'Domain Driven Danish Pastry',
+    cost  : 750
+  },
+
+  {
+    title : 'Design Pattern bacon',
+    cost  : 800 
+  }
+  
+];
+
 exports.showCourses = (req, res) => {
-  res.render('courses');
+  res.render('courses', { 
+    offeredCourses: courses 
+  });
 };
 
 exports.showSignUp = (req, res) => {
