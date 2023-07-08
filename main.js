4a2791aebc2b9fdd26ef3b75cf879a073a41d074
toonMarty <martinnyaga7373@gmail.com>
2023-07-08 13:02:27 +0300

    Serving static views

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 /**App logic */
 
 const express = require('express');
+const homeController = require('./controllers/homeController');
+const layouts = require('express-ejs-layouts');
 const app = express();
 
 /**Middleware to interpret incoming request bodies */
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(layouts);
 
 app.set("port", process.env.PORT || 3000);
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.send('Willkommen bei Meal Maker');
-});
+app.get('/', homeController.showCourses);
+app.get('/contact', homeController.showSignUp);
+app.post('/contact', homeController.postedSignUpForm);
 
 app.listen(app.get('port'), () => {
   console.log(`Server running at http://localhost:${app.get('port')}`);
