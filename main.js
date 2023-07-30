3285adbc5c7ed0072a658ac75cd4ba341522fa6c
toonMarty <martinnyaga7373@gmail.com>
2023-07-30 13:26:43 +0300

    Create application routes for user, course, and subscriber models

--- main.js
+++ main.js
@@ -1,34 +1,72 @@
 /**App logic */
 
 const express = require('express');
+const router = express.Router();
 
 const homeController = require('./controllers/homeController');
 const errorController = require('./controllers/errorController');
 const subscribersController = require('./controllers/subscribersController');
+const usersController = require('./controllers/usersController');
+const coursesController = require('./controllers/coursesController');
 
 const layouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/meal_maker', {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
-let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 /**Middleware to interpret incoming request bodies */
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(layouts);
+app.use('/', router);
+
+router.use(methodOverride('_method', {'methods': ['GET', 'POST']}));
 
 app.set("port", process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 
-app.get('/', homeController.showCourses);
-app.get('/contact', subscribersController.getSubscriptionPage);
-app.get('/subscribers', subscribersController.getAllSubscribers);
+router.get('/', homeController.showCourses);
 
-app.post('/subscribe', urlencodedParser, subscribersController.saveSubscriber);
+// router.get('/contact', subscribersController.new);
+router.get('/subscribers', subscribersController.index, subscribersController.indexView);
+router.get('/subscribers/new', subscribersController.new, subscribersController.redirectView);
+router.get('/subscribers/:id', subscribersController.show, subscribersController.showView);
+router.get('/subscribers/:id/edit', subscribersController.edit);
+
+router.put('/subscribers/:id/update', subscribersController.update, subscribersController.redirectView);
+
+router.delete('/subscribers/:id/delete', subscribersController.delete, subscribersController.redirectView);
+
+
+// router.post('/subscribe', subscribersController.create);
+router.post('/subscribers/create', subscribersController.create, subscribersController.redirectView);
+
+router.get('/users', usersController.index, usersController.indexView);
+router.get('/users/new', usersController.new);
+router.get('/users/:id', usersController.show, usersController.showView);
+router.get('/users/:id/edit', usersController.edit);
+
+router.post('/users/create', usersController.create, usersController.redirectView);
+
+router.put('/users/:id/update', usersController.update, usersController.redirectView);
+
+router.delete('/users/:id/delete', usersController.delete, usersController.redirectView);
+
+router.get('/courses', coursesController.index, coursesController.indexView);
+router.get('/courses/new', coursesController.new, coursesController.redirectView);
+router.get('/courses/:id', coursesController.show, coursesController.showView);
+router.get('/courses/:id/edit', coursesController.edit);
+
+router.post('/courses/create', coursesController.create, coursesController.redirectView);
+
+router.put('/courses/:id/update', coursesController.update, coursesController.redirectView);
+
+router.delete('/courses/:id/delete', coursesController.delete, coursesController.redirectView);
 
 
 
