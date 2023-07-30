70bce4397bb96495d8fe8f790985452e7b4b5b87
toonMarty <martinnyaga7373@gmail.com>
2023-07-30 13:34:03 +0300

    [feat]: Implement the user module and CRUD functionality

---
+++ models/user.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+const Subscriber = require('./subscriber');
+
+const userSchema = new Schema({
+  name: {
+    first: {
+      type  : String,
+      trim  : true
+    },
+
+    last: {
+      type: String,
+      trim: true
+    }
+  },
+
+  email: {
+    type        : String,
+    required    : true,
+    lowercase   : true,
+    unique      : true
+  },
+
+  zipCode: {
+    type        : Number,
+    min         : [10000, "Zip code too short"],
+    max         : 99999
+  },
+
+  password: {
+    // needs to be hashed for security.
+    type        : String,
+    required    : true
+  },
+
+  courses: [{
+    type    : Schema.Types.ObjectId,
+    ref     : "Course"
+  }],
+
+  subscribedAccount: {type: Schema.Types.ObjectId, ref: "Subscriber"}
+},
+
+{
+    timestamps: true
+});
+
+userSchema.virtual("fullName")
+.get(function() {
+  return `${this.name.first} ${this.name.last}`
+});
+
+userSchema.virtual("letterCount")
+.get(function() {
+  let nameLength = this.name.first.length + this.name.last.length;
+  return nameLength;
+});
+
+userSchema.virtual('firstLetter')
+.get(function() {
+  return this.name.first[0];
+});
+
+userSchema.pre("save", function(next) {
+  /**
+   * Find an association between a user and a
+   * subscriber before saving a user to the
+   * database
+   */
+  let user = this;
+
+  if (user.subscribedAccount === undefined) {
+    Subscriber.findOne({
+      email: user.email
+    })
+    .then(subscriber => {
+      user.subscribedAccount = subscriber;
+      next();
+    })
+    .catch(error => {
+      console.log(`Error connecting subscriber: ${error.message}`);
+      next(error);
+    })
+  } else {
+    next();
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
