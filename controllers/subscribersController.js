516977646322704f948e0e9cf9bf2f4e55839145
toonMarty <martinnyaga7373@gmail.com>
2023-07-30 13:37:44 +0300

    [feat]: Implement the Subscriber module with CRUD functionality

--- controllers/subscribersController.js
+++ controllers/subscribersController.js
@@ -3,37 +3,118 @@
  * for subscribers
  */
 
+const subscriber = require('../models/subscriber');
 const Subscriber = require('../models/subscriber');
 
-exports.getAllSubscribers = (req, res) => {
+const getSubscriberParams = (body) => {
+  /**
+   * get subscriber data from request
+   */
+  return {
+    name    : body.name,
+    email   : body.email,
+    zipCode : parseInt(body.zipCode)
+  }
+}
+
+module.exports = {
+  index: (req, res, next) => {
     // find and display all subscribers
     Subscriber.find({})
-    .then((subscribers) => {
-        res.render('subscribers', {subscribers: subscribers});
+    .then(subscribers => {
+      res.locals.subscribers = subscribers;
+      next();
     })
     .catch((err) => {
-        console.log(err.message);
-        return [];
+        console.log(`Error fetching subscribers: ${err.message}`);
+        next(err);
     });
-}
+  },
 
-exports.getSubscriptionPage = (req, res) => {
+  indexView: (req, res) => {
+    res.render('subscribers/index');
+  },
+
+  new: (req, res) => {
     // get the newsletter subscription page
-    res.render('contact');
-}
+    res.render('subscribers/new');
+  },
 
-exports.saveSubscriber = (req, res) => {
-    const newSubscriber = new Subscriber({
-      name      : req.body.name,
-      email     : req.body.email,
-      zipCode   : req.body.zipCode
-    });
-
-    newSubscriber.save()
-    .then(() => {
-      res.render('thanks');
+  create: (req, res, next) => {
+    let subscriberParams = getSubscriberParams(req.body);
+    Subscriber.create(subscriberParams)
+    .then(subscriber => {
+      res.locals.redirect = '/subscribers';
+      res.locals.subscriber = subscriber;
+      next();
     })
-    .catch(err => {
-       res.send(err);
+    .catch(error => {
+      console.log(`Failed to save subscriber: ${error.message}`);
+      next(error);
     });
+  },
+
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath) res.redirect(redirectPath);
+    else next();
+  },
+
+  show: (req, res, next) => {
+    let subscriberId = req.params.id;
+    Subscriber.findById(subscriberId)
+    .then(subscriber => {
+      res.locals.subscriber = subscriber;
+      next();
+    })
+    .catch(error => {
+      console.log(`Couldn't find subscriber: ${error.message}`);
+      next(error);
+    })
+  },
+
+  showView: (req, res) => {
+    res.render('subscribers/show');
+  },
+
+  edit: (req, res, next) => {
+    let subscriberId = req.params.id;
+    Subscriber.findById(subscriberId)
+    .then(subscriber => {
+      res.render('subscribers/edit', {subscriber: subscriber});
+    })
+    .catch(error => {
+      console.log(`Error finding subscriber: ${error.message}`);
+      next(error);
+    })
+  },
+
+  update: (req, res, next) => {
+    let subscriberId = req.params.id;
+    let subscriberParams = getSubscriberParams(req.body);
+
+    Subscriber.findByIdAndUpdate(subscriberId, { $set: subscriberParams})
+    .then(subscriber => {
+      res.locals.redirect = `/subscribers/${subscriberId}`;
+      res.locals.subscriber = subscriber;
+      next();
+    })
+    .catch(error => {
+      console.log(`Couldn't update subscriber: ${error.message}`);
+      next(error);
+    });
+  },
+
+  delete: (req, res, next) => {
+    let subscriberId = req.params.id;
+    Subscriber.findByIdAndRemove(subscriberId)
+    .then(() => {
+      res.locals.redirect = '/subscribers';
+      next();
+    })
+    .catch(error => {
+      console.log(`Couldn't update subscriber: ${error.message}`);
+      next();
+    });
+  }
 }
--- models/subscriber.js
+++ models/subscriber.js
@@ -1,13 +1,58 @@
 /**
  * This module defines the subscriber model data
+ * along with data validation
  */
 
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const subscriberSchema = mongoose.Schema({
-    name    : String,
-    email   : String,
-    zipCode : Number
+const subscriberSchema = new Schema({
+    name: {
+      type: String,
+      required: true
+    },
+
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true
+    },
+
+    zipCode: {
+      type: Number,
+      min: [10000, "Zip code too short"],
+      max: 99999
+    },
+
+    courses: [
+      {
+        type  : mongoose.Schema.Types.ObjectId,
+        ref   : 'Course'
+      }
+    ]
+},
+
+{
+  timestamps: true
 });
+
+subscriberSchema.methods.getInfo = function() {
+  /** 
+   * get information about a particular subscriber, i.e
+   * a Subscriber instance
+   */
+
+  return `Name      : ${this.name}, 
+          Email     : ${this.email}, 
+          Zip Code  : ${this.zipCode}`
+}
+
+subscriberSchema.methods.findLocalSubscribers = function() {
+  // find subscribers with the same zipcode, i.e from the same area
+  return this.model('Subscriber')
+  .find({zipCode: this.zipCode})
+  .exec()
+}
 
 module.exports = mongoose.model('Subscriber', subscriberSchema);
